6822b02e6c8f69a0f390c78e3012d6f412b2d8ce
duncanjohanne <70613947+duncanjohanne@users.noreply.github.com>
2025-09-01 16:21:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,12 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
+    allowedHosts: [
+      'portfolio.innaesim.com',
+      'localhost',
+      '127.0.0.1'
+    ]
   },
   plugins: [
     react(),
